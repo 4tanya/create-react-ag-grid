7326e9a636d6e6e5ba6a52bfc5269e9fa44410e5
Tatyana Korzun <tatyana.korzun@codex-soft.com>
2019-02-03 02:28:23 +0300

    create-react-app with ag-grid

--- src/App.js
+++ src/App.js
@@ -1,27 +1,110 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-enterprise';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        columnDefs: [
+          {
+            headerName: "Athlete Details",
+            children: [
+              {
+                headerName: "Athlete", 
+                field: "athlete",
+                width: 200,
+                checkboxSelection: true
+              },
+              {headerName: "Age", field: "age"},
+              {headerName: "Country", field: "country", rowGroup: true, rowGroupIndex: 1, hide: true},
+            ]
+          },
+          {
+              headerName: "Sports Results",
+              children: [
+                  {headerName: "Year", field: "year"},
+                  {headerName: "Date", field: "date"},
+                  {headerName: "Sport", field: "sport", width: 170, rowGroup: true, rowGroupIndex: 0, hide: true},
+                  {headerName: "Gold", field: "gold"},
+                  {headerName: "Silver", field: "silver"},
+                  {headerName: "Bronze", field: "bronze"},
+                  {headerName: "Total", field: "total"}
+              ]
+          }
+        ],
+        defaultColDef: {
+          width: 90,
+          editable: true,
+          enableValue: true,
+          sortable: true,
+          resizable: true,
+          filter: true
+        },
+        defaultGroupSortComparator: function(nodeA, nodeB) {
+          if (nodeA.key < nodeB.key) {
+            return -1;
+          } else if (nodeA.key > nodeB.key) {
+            return 1;
+          } else {
+            return 0;
+          }
+        },
+        paginationPageSize: 15,
+        rowBuffer: 15,
+        rowSelection: "multiple"
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://raw.githubusercontent.com/ag-grid/ag-grid/master/packages/ag-grid-docs/src/olympicWinners.json")
+      .then(result => result.json())
+      .then(rowData => this.updateData(rowData))
+  }
+
+  onGridReady = params => {
+    this.gridApi = params.api;
+
+    this.updateData = data => {
+      this.setState({ rowData: data });
+    };
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="c-ag-greed">
+        <div className="ag-theme-balham-dark c-ag-greed__table-wrapper">
+            <button onClick={this.onButtonClick} className="c-ag-greed__button">Get selected rows</button>
+            <div className="c-ag-greed__selected-rows-output"></div>
+            <AgGridReact
+                columnDefs={this.state.columnDefs}
+                defaultColDef={this.state.defaultColDef}
+                paginationPageSize={this.state.paginationPageSize}
+                pagination={true}
+                rowBuffer={this.state.rowBuffer}              
+                allowContextMenuWithControlKey={true}
+                groupUseEntireRow={true}
+                defaultGroupSortComparator={this.state.defaultGroupSortComparator}
+                rowSelection={this.state.rowSelection}
+                onGridReady={this.onGridReady}
+                rowData={this.state.rowData}>
+            </AgGridReact>
+        </div>
       </div>
     );
+  }
+
+  onButtonClick = e => {
+    const selectedNodes = this.gridApi.getSelectedNodes();
+    const selectedData = selectedNodes.map( node => node.data );
+    const selectedDataStringPresentation = selectedData.map( 
+      node => 'Athlete: ' + node.athlete + ', Sport: ' + node.sport).join('<br/>');
+    document.querySelector(".c-ag-greed__selected-rows-output").innerHTML = (!selectedData.length)?
+      'No nodes selected'
+      : `Selected nodes:<br /> ${selectedDataStringPresentation}`; 
   }
 }
 
